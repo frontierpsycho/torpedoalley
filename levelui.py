92f42e53773088c57242ffc376a1b518af25bed0
Nameless <unfortunate42@gmail.com>
2012-10-06 01:50:16 +0300

    Rearranged members, prepared for level transition.

--- levelui.py
+++ levelui.py
@@ -24,7 +24,8 @@ class LevelUI(threading.Thread):
 		self.start()
 
 	def run(self):
-		self.root=Tk()
+		if not hasattr(self, "root"):
+			self.root=Tk()
 		self.root.protocol("WM_DELETE_WINDOW", self.destroy_confirm)
 
 		self.canvas = Canvas(self.root, width=800, height=600)
@@ -66,6 +67,7 @@ class LevelUI(threading.Thread):
 		
 		self.root.mainloop()
 
+	### custom main loop to receive commands from game logic
 	def check_input(self):
 		try:
 			event = self.in_queue.get_nowait()
@@ -96,24 +98,8 @@ class LevelUI(threading.Thread):
 
 	def level_complete(self):
 		self.canvas.create_text(400,300, text="Level complete!", fill="white", font=("Arial", 16, "bold"))
-
-	def destroy_confirm(self):
-		if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
-			self.out_queue.put("quit")
-			self.root.destroy()
-
-	def keyPressed(self, event):
-		if event.keysym in self.directions:
-			if self.stopped:
-				self.stopped = False
-				self.steer(event.keysym)
-
-		self.out_queue.put(event.keysym)
-
-	def keyReleased(self, event):
-		if event.keysym in self.directions:
-			self.stop_submarine()
-
+		self.canvas.bind("<Return>", self.close)
+	
 	def place_torpedo(self):
 		sub_coords = self.canvas.coords(self.submarine)
 
@@ -128,6 +114,24 @@ class LevelUI(threading.Thread):
 
 		return sub_coords
 
+	### item movement functions ######################################################################################
+	def check_underwater(self, object):
+		coords = self.canvas.coords(self.sea)
+		return (object in self.canvas.find_enclosed(*coords))
+
+	def stop_submarine(self):
+		self.stopped = True
+
+	def move_ship(self, ship_id, speed):
+		self.canvas.move(ship_id, speed, 0)
+
+		if ship_id not in self.canvas.find_overlapping(*self.canvas.coords(self.sky)):
+			self.canvas.delete(ship_id)
+			if self.completed and not self.canvas.find_withtag("ship"):
+				self.level_complete()
+		else:
+			self.root.after(30, self.move_ship, ship_id, speed)
+
 	def torpedo_deltas(self, torpedo_coords, dest_coords):
 		torpedo_center = graphics_helpers.circle_center_from_bbox(5, torpedo_coords)
 
@@ -137,13 +141,45 @@ class LevelUI(threading.Thread):
 		# dx, dy and desired speed form a right triangle
 		return graphics_helpers.deltas_for_speed(7, tx, ty)
 
-	### item movement functions ###
-	def check_underwater(self, object):
-		coords = self.canvas.coords(self.sea)
-		return (object in self.canvas.find_enclosed(*coords))
+	def steer(self, direction, speed=2):
+		if direction == "Left":
+			dx, dy = (-speed, 0)
+		elif direction == "Right":
+			dx, dy = (speed, 0)
+		elif direction == "Up":
+			dx, dy = (0, -speed)
+		elif direction == "Down":
+			dx, dy = (0, speed)
+		else:
+			# panic! Just kidding.
+			logger.error("Received %s and don't know how to handle it!" % direction)
+	
+		self.canvas.move(self.submarine, dx, dy)
+		if not self.check_underwater(self.submarine):
+			self.canvas.move(self.submarine, -dx, -dy)
+			self.stopped = True
+	
+		if not self.stopped:
+			self.root.after(30, self.steer, direction, speed)
 
-	def stop_submarine(self):
-		self.stopped = True
+	### member functions that send events to the main loop ###########################################################
+	def keyPressed(self, event):
+		if event.keysym in self.directions:
+			if self.stopped:
+				self.stopped = False
+				self.steer(event.keysym)
+
+		self.out_queue.put(event.keysym)
+
+	def launch_send(self, mouseclick):
+		if mouseclick.y < self.canvas.coords(self.submarine)[1]:
+			# only destinations over the sub are valid
+			self.out_queue.put("launch %d,%d" % (mouseclick.x, mouseclick.y))
+
+	def destroy_confirm(self):
+		if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
+			self.out_queue.put("quit")
+			self.root.destroy()
 
 	def move_torpedo(self, torpedo_id, dx, dy):
 		coords = self.canvas.coords(torpedo_id)
@@ -168,44 +204,15 @@ class LevelUI(threading.Thread):
 			# if torpedo left the sea, delete it
 			self.canvas.delete(torpedo_id)
 
-	def move_ship(self, ship_id, speed):
-		self.canvas.move(ship_id, speed, 0)
+	def close(self, event):
+		self.canvas.destroy()
+		self.out_queue.put("complete")
 
-		if ship_id not in self.canvas.find_overlapping(*self.canvas.coords(self.sky)):
-			self.canvas.delete(ship_id)
-			if self.completed and not self.canvas.find_withtag("ship"):
-				self.level_complete()
-		else:
-			self.root.after(30, self.move_ship, ship_id, speed)
+	### member functions that display events received from the main loop ################################################
+	def keyReleased(self, event):
+		if event.keysym in self.directions:
+			self.stop_submarine()
 
-	def steer(self, direction, speed=2):
-		if direction == "Left":
-			dx, dy = (-speed, 0)
-		elif direction == "Right":
-			dx, dy = (speed, 0)
-		elif direction == "Up":
-			dx, dy = (0, -speed)
-		elif direction == "Down":
-			dx, dy = (0, speed)
-		else:
-			# panic! Just kidding.
-			logger.error("Received %s and don't know how to handle it!" % direction)
-	
-		self.canvas.move(self.submarine, dx, dy)
-		if not self.check_underwater(self.submarine):
-			self.canvas.move(self.submarine, -dx, -dy)
-			self.stopped = True
-	
-		if not self.stopped:
-			self.root.after(30, self.steer, direction, speed)
-
-	### member functions that send events to the main loop ###
-	def launch_send(self, mouseclick):
-		if mouseclick.y < self.canvas.coords(self.submarine)[1]:
-			# only destinations over the sub are valid
-			self.out_queue.put("launch %d,%d" % (mouseclick.x, mouseclick.y))
-
-	### member functions that display events received from the main loop ###
 	def launch(self, event):
 		torpedo_coords = self.place_torpedo()
 
